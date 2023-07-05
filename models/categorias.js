eb5ab4a47c2ce9f027d58e29c13fab8c060e32c5
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-07-04 20:07:47 -0400

    Capa modelo lista

---
+++ models/categorias.js
@@ -0,0 +1,33 @@
+export class Categorias{
+
+    constructor(categoriaId, categoriaNombre, descripcion){
+        this._categoriaId = categoriaId;
+        this._categoriaNombre = categoriaNombre;
+        this._descripcion = descripcion;
+    }
+
+    get categoriaId(){
+        return this._categoriaId;
+    }
+
+    set categoriaId(categoriaId){
+        this._categoriaId = categoriaId;
+    }
+
+    get categoriaNombre(){
+        return this._categoriaNombre;
+    }
+
+    set categoriaNombre(categoriaNombre){
+        this._categoriaNombre = categoriaNombre;
+    }
+
+    get descripcion(){
+        return this._descripcion;
+    }
+
+    set descripcion(descripcion){
+        this._descripcion;
+    }
+
+}
---
+++ models/productos.js
@@ -0,0 +1,60 @@
+export class Productos{
+
+    constructor(productoId, productoNombre, categoriaId, precioUnitario, unidadesStock, discontinuado){
+        this._productoId = productoId;
+        this._productoNombre = productoNombre;
+        this._categoriaId = categoriaId;
+        this._precioUnitario = precioUnitario;
+        this._unidadesStock = unidadesStock;
+        this._discontinuado = discontinuado;
+    }
+
+    get productoId(){
+        return this._productoId;
+    }
+
+    set productoId(productoId){
+        this._productoId = productoId;
+    }
+
+    get productoNombre(){
+        return this._productoNombre;
+    }
+
+    set productoNombre(productoNombre){
+        this._productoNombre = productoNombre;
+    }
+
+    get categoriaId(){
+        return this._categoriaId;
+    }
+
+    set categoriaId(categoriaId){
+        this._categoriaId = categoriaId;
+    }
+
+    get precioUnitario(){
+        return this._precioUnitario;
+    }
+
+    set precioUnitario(precioUnitario){
+        this._precioUnitario = precioUnitario;
+    }
+
+    get unidadesStock(){
+        return this._unidadesStock;
+    }
+
+    set unidadesStock(unidadesStock){
+        this._unidadesStock = unidadesStock;
+    }
+
+    get discontinuado(){
+        return this._discontinuado;
+    }
+
+    set discontinuado(discontinuado){
+        this._discontinuado = discontinuado;
+    }
+
+}
